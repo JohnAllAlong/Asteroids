648f27ef0040c439c0a4446860c133ccda0afae6
John Carlson <johnathonccontact@gmail.com>
2025-03-12 11:11:41 -0500

    Cleaned up Sketch file
    I now have a better gameplan for referencing instances of objects, will implement next.

--- Asteroids/sketch.js
+++ Asteroids/sketch.js
@@ -3,85 +3,71 @@
 //UP ARROW TO FIRE ENGINE
 //SPACEBAR TO SHOOT
 
-let player;
-let asteroid;
-let ya;
-let asteroids = [];
-let startNum = 5;
-let currentNum = 0;
-let children = 2;
-let heading = 0;
-let startPosition
-
 let spawner;
+let player;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  player = new PlayerShip(windowWidth / 2, windowHeight / 2);  
-  spawner = new Spawner()
-  spawner.spawnAsteroids()
+  spawner = new Spawner();
+  spawner.spawnPlayerShip();
+  player = spawner.player;
+  spawner.spawnAsteroids();
 }
 
 function draw() {
   background(10);
   frameRate(60);
-  
 
-  
-  
   player.update();
   player.display();
-  detectInput()
+  detectInput();
 
-  
-
-  if(spawner.projectiles.length != 0){
-    for(let k = 0; k < spawner.projectiles.length; ++k){
-    spawner.projectiles[k].display()
-    spawner.projectiles[k].update()
-    console.log(spawner.projectiles.length)
+  if (spawner.projectiles.length != 0) {
+    for (let k = 0; k < spawner.projectiles.length; ++k) {
+      spawner.projectiles[k].display();
+      spawner.projectiles[k].update();
+      console.log(spawner.projectiles.length);
     }
   }
 
-  if(spawner.asteroids.length != 0){
-  for(let j = 0; j < spawner.asteroids.length; ++j){
-    spawner.asteroids[j].display()
-    spawner.asteroids[j].update()
+  if (spawner.asteroids.length != 0) {
+    for (let j = 0; j < spawner.asteroids.length; ++j) {
+      spawner.asteroids[j].display();
+      spawner.asteroids[j].update();
+    }
   }
-}
- 
 }
 
-function keyPressed(){
-  if(key === 'j'){
-    spawner.spawnAsteroids()
+function keyPressed() {
+  if (key === "j") {
+    spawner.spawnAsteroids();
   }
-  if(key === 'k'){
-  spawner.destroyAsteroid(0)
-}
+  if (key === "k") {
+    spawner.destroyAsteroid(0);
+  }
 }
 
 function detectInput() {
-  if(player != null){
-  if (keyIsDown("65")) {
-    player.updateRotation(-1);
-  }
-  if (keyIsDown("68")) {
-    player.updateRotation(1);
-  }
-  if (keyIsDown(UP_ARROW)) {
-    player.isEngineActive = true;
-  } else {
-    player.isEngineActive = false;
-  }
-  if (keyIsDown("32")) {
-    player.getBulletInfo();
-    if (player.currentBullets < player.maxBullets && player.canFire){
-    spawner.spawnProjectile(player.position, player.target, 40)
+  if (player != null) {
+    if (keyIsDown("65")) {
+      player.updateRotation(-1);
     }
-    player.canFire = false;
-  } else {
-    player.canFire = true;
+    if (keyIsDown("68")) {
+      player.updateRotation(1);
+    }
+    if (keyIsDown(UP_ARROW)) {
+      player.isEngineActive = true;
+    } else {
+      player.isEngineActive = false;
+    }
+    if (keyIsDown("32")) {
+      player.getBulletInfo();
+      if (player.currentBullets < player.maxBullets && player.canFire) {
+        spawner.spawnProjectile(player.position, player.target, 40);
+      }
+      player.canFire = false;
+    } else {
+      player.canFire = true;
+    }
   }
-}
 }
--- Asteroids/spawner.js
+++ Asteroids/spawner.js
@@ -1,40 +1,48 @@
-class Spawner{
+class Spawner {
+  constructor() {
+    this.asteroids = [];
+    this.startNum = 5;
+    this.projectiles = [];
+  }
 
-constructor(){
-this.asteroids = []
-this.startNum = 5
-this.projectiles = []
-}
-
-spawnAsteroids(){
-    for(let i = 0; i < this.startNum; ++i){
-
-        this.heading = random(0, TWO_PI)
-        this.startPosition = createVector(random(0, windowWidth), random(0, windowHeight))
-        this.asteroid = new Asteroid(this.startPosition, this.heading, 1)
-        this.asteroids.push(this.asteroid)
-        //console.log(asteroids.length)
-      }
-}
-
-destroyAsteroid(index){
-    for(let i = 0; i < 2; ++i){
-        this.heading = random(0, TWO_PI)
-        this.pozish = this.asteroids[index].startPos.copy()
-
-        if(this.asteroids[index].sizeMult >= 3){
-            this.asteroids.splice(this.asteroids[index], 1)
-            break;
-          }
-        this.asteroid = new Asteroid(this.pozish, this.heading, this.asteroids[index].sizeMult + 1)
-        this.asteroids.push(this.asteroid)
+  spawnAsteroids() {
+    for (let i = 0; i < this.startNum; ++i) {
+      this.heading = random(0, TWO_PI);
+      this.startPosition = createVector(
+        random(0, windowWidth),
+        random(0, windowHeight)
+      );
+      this.asteroid = new Asteroid(this.startPosition, this.heading, 1);
+      this.asteroids.push(this.asteroid);
+      //console.log(asteroids.length)
     }
-    this.asteroids.splice(this.asteroids[index], 1)
-}
+  }
 
-spawnProjectile(position, target, size){
+  destroyAsteroid(index) {
+    for (let i = 0; i < 2; ++i) {
+      this.heading = random(0, TWO_PI);
+      this.pozish = this.asteroids[index].startPos.copy();
+
+      if (this.asteroids[index].sizeMult >= 3) {
+        this.asteroids.splice(this.asteroids[index], 1);
+        break;
+      }
+      this.asteroid = new Asteroid(
+        this.pozish,
+        this.heading,
+        this.asteroids[index].sizeMult + 1
+      );
+      this.asteroids.push(this.asteroid);
+    }
+    this.asteroids.splice(this.asteroids[index], 1);
+  }
+
+  spawnProjectile(position, target, size) {
     this.bullet = new Projectile(position, target, size);
     this.projectiles.push(this.bullet);
-}
+  }
 
+  spawnPlayerShip() {
+    this.player = new PlayerShip(windowWidth / 2, windowHeight / 2);
+  }
 }
